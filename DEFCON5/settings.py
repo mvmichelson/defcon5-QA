d2c822a0610694cafc08101433ef29334e8e511a
Marco Villalobos <solution.work2023@gmail.com>
2025-06-21 21:11:40 -0400

    Agrego ruta TEMPLATE para que se encuentren los Templates de login

--- DEFCON5/settings.py
+++ DEFCON5/settings.py
@@ -57,6 +57,11 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'bcp', 'templates')],
+
+        'DIRS': [ os.path.join(BASE_DIR, 'bcp', 'templates'),
+                 os.path.join(BASE_DIR, 'templates'),
+                 ],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
